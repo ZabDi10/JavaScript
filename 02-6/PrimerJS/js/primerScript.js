511a31b81541a41622be963edac8c844c2023cf0
ZabDi10 <keko10@hotmail.es>
2023-06-02 20:54:31 +0200

    02-6

--- 02-6/PrimerJS/js/primerScript.js
+++ 02-6/PrimerJS/js/primerScript.js
@@ -17,8 +17,8 @@ document.getElementById("demo").innerHTML = "Hello JavaScript"
 // Variables tipo entero
 let number1 = 45;
 let number2 = 25;
-let suma = number1 +number2;
-console.log(suma);
+let sumar = number1 +number2;
+console.log(sumar);
 let apellido = "PONE <strong>PACIOS</strong> no palacios"
 nombre += " " + apellido;
 console.log(nombre);
@@ -28,4 +28,43 @@ document.getElementById("nuevoNombre").innerHTML = nombre;
 
 numero3 = "tres";
 suma1 = number1 + numero3;
-console.log(suma1);
+console.log(suma1); // es una manera de visualizar una salida en javascript
+
+let numero = 1;
+// crear mi primera funcion -> una serie de instrucciones que se ejecutaran cuando sea llamada
+function mostrarAlerta(consola){
+    console.log(consola);
+    if(numero === 1){
+        numero = 2;
+        alert("HOLIIIIIIIIIIIII");
+        alert("Vale me he calentado");
+        nombre = "Diego Zaballos Pacios";
+        document.getElementById("nuevoNombre").innerHTML = nombre;
+        document.getElementById("boton1").innerHTML = "Cambiame Aghhhh!!!!";
+    }else{
+        numero = 1;
+        alert("SE VIENE EL LOREM");
+        document.getElementById("nuevoNombre").innerHTML = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, enim! Provident accusamus laudantium alias deserunt esse fugiat fuga voluptates dolor reiciendis, velit aliquam iusto beatae voluptate facere laborum pariatur iste.";
+        document.getElementById("boton1").innerHTML = "Click!";
+    }
+}
+//Otra manera de llamarlo --> escucha de eventos
+document.getElementById('boton1').onclick =  () => {
+    alert('Voy antes jejjeejejej');
+    mostrarAlerta('Valor. Valor. Valor hasiendo la pasasion al console logo');
+}
+
+/* Paso de parametros a una funcion
+*/
+document.getElementById("sumar1").innerHTML = sumar1(45,34);
+
+function sumar1(a,b) {
+    return a + b;
+}
+
+function sumar2(){
+    let x = document.getElementById("numero1").value;
+    let y = document.getElementById("numero2").value;
+    document.getElementById("sumarResultado").innerHTML = `El valor de la x es: ${x}. El valor de la y es: ${y}. La suma es: ${sumar1(parseInt(x),parseInt(y))}`
+}
+    
