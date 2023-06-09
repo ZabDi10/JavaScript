5c8ca2e37293012d489a383f74b368a963a758b4
ZabDi10 <keko10@hotmail.es>
2023-06-09 16:14:24 +0200

    Recuperacion 03-6

--- 02-6/PrimerJS/js/primerScript.js
+++ 02-6/PrimerJS/js/primerScript.js
@@ -67,4 +67,109 @@ function sumar2(){
     let y = document.getElementById("numero2").value;
     document.getElementById("sumarResultado").innerHTML = `El valor de la x es: ${x}. El valor de la y es: ${y}. La suma es: ${sumar1(parseInt(x),parseInt(y))}`
 }
+
+// existe otra manera de declarar las variable <2015
+var nombreAlumno= "Rosa Huertas";
+console.log(nombreAlumno);
+// diferencia entre var y let
+//1. Se puede volver a declarar la misma variable
+var nombreAlumno="José Martinez"
+console.log(nombreAlumno);
+//ejemplo con let
+let nombrePadre="Miguel García";
+console.log(nombrePadre);
+// no se puede declarar de nuevo una variable con let
+//en el mismo bloque de codigo
+function otroNombre(){
+    let nombrePadre="Gonzalo León";
+    console.log("Dentro de la función-> " +nombrePadre);
+}
+otroNombre();// llamamos la función para que se ejecute
+console.log("Fuera de la función-> "+nombrePadre);
+// en la declaración de las variables se puede declarar una o más variables en la misma linea
+//var nombreMadre;//declaración-> indicando que nombreMadre es una variable
+let nombreMadre="Sofia Luna", nombreNino="Fiona Martinez", colegio="Cervantes";
+//las variables no estas obligado a declararlas, pero es importante y buena practica
+edad=15; // tipo de números entero...
+console.log(edad);
+precio=5.8;// decimal
+console.log(precio);
+edad=18;//reemplaza el valor
+console.log(edad);
+nombreMadre="Rosa Díaz";  //le asigno a la variable nombreMadre en valor de Rosa Díaz
+console.log(nombreMadre);
+/*
+ lo que se guarda en una variable
+ tipos-> números (5, 4, 5.9, etc)
+         cadena de caracteres ("El nombre es")
+         booleanos (true, false)->(verdadero, falso)
+*/
+let miBooleano=true; //on / off - 0/1
+//las cadenas de caracteres existe la limitante de utilizar "" dentro de una cadena
+//para ello exite Caracteres de escape en cadenas de texto
+let cadena="Esta es una cadena\" muy importante\" de javaScript";
+console.log(cadena);
+// puedes combinar las dobles comillas con las comillas simples
+let cadena2='Esta es la segunda cadena "muy importante"';
+console.log(cadena2);
+cadena="Esta es una cadena 'muy importante' de javaScript";
+console.log(cadena);
+//salto de linea con caracteres de escape \n
+cadena="Esta es una cadena 'muy importante' \nde javaScript";
+console.log(cadena);
+
+/* Operadores Matemáticos
+    + - * /-> Operaciones básicas
+    %-> móduloo resto de dos números
+*/
+x=10;
+y=2;
+let modulo=10%2;// da como resultado el resto de la división
+console.log("El módulo entre 10/2 es: " +modulo);
+let operacion=(x+2)*2-(y/3);
+console.log(operacion);
+/*
+Operadores de asignación
+    = asigna
+    += incrementa el numero 
+    -= decrementa
+    *= multiplica
+    /= divide
+*/
+x+=2; //x=12
+console.log(x);
+x-=2; //x=10
+console.log(x);
+x*=2;
+console.log(x);
+x/=2;
+console.log(x);
+
+//se puede utilizar el operador de asignación += eb las cadenas de caracteres
+cadena+=cadena2;
+console.log(cadena);
+//Operador exponencial
+x=y**10
+console.log(x);
+//Operador incremento
+++y;
+console.log(y);
+//Operador decremento
+--y;
+//salida en pantalla directamente insertando el texto en la última
+//línea disponible.
+document.write("El valor de y es "+y);
+
+/*
+        - Constantes -
+    No cambian en el resto del programa, se declara con la "palabra reservada" const y se debe asignar su valor cuando se declara
+
+    palabras reservadas (palabras clave)= propias del lenguaje 
+    let, var, const...
+*/
+const PI=3.141618;//las constantes se declaran en mayusculas
+const MONEDA=5;
+//área de un circulo area=r**2xPI
+let area=(50**2)*PI;
+console.log("El área de una circunferencia de radio 50 es " +area);
     
